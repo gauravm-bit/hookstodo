0adf44ea9d15298da1d309038761f1e2a57e4286
gauravm-bit <gauravwrk01@gmail.com>
2021-01-29 20:56:02 +0530

    ADDED : input and header for todo list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import './App.css'
+import React,{useState} from 'react';
+import shortid from 'shortid';
+import './App.css';
+
 function App() {
+
+  const[todo, setTodo] = useState({text : ""}) 
+  const[list,setList] = useState([])
+
+  const handleChange = (event) => {
+      setTodo({
+        todo : {text : event.target.value}
+      })
+  }
+ console.log(todo);
+  
+ const handleSubmit = (event) => {
+   event.preventDefault()
+     setList({
+       list : [...list,todo]
+     })
+  }
+  console.log(list);
+
   return (
     <div className = "app">
      <header id ="header">
        TO-DO LIST 
      </header>
+     <form onSubmit={handleSubmit}>
+       <input placeholder="Enter Here"
+              onChange = {handleChange}
+       ></input>
+       <button onClick={handleSubmit}  >+</button>
+     </form>
     </div>
   );
 }
