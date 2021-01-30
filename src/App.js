1094343586137096a0792f8fa730dbcf9a55a0b4
gauravm-bit <gauravwrk01@gmail.com>
2021-01-30 15:20:09 +0530

    ADDED: add todo to list

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React,{useState} from 'react';
-import shortid from 'shortid';
-import './App.css';
+import React, { useState } from "react";
+import shortid from "shortid";
+import "./App.css";
 
 function App() {
-
-  const[todo, setTodo] = useState({text : ""}) 
-  const[list,setList] = useState([])
+  const [todo, setTodo] = useState({ text: "" });
+  const [list, setList] = useState([]);
 
   const handleChange = (event) => {
-      setTodo({
-        todo : {text : event.target.value}
-      })
-  }
- console.log(todo);
-  
- const handleSubmit = (event) => {
-   event.preventDefault()
-     setList({
-       list : [...list,todo]
-     })
-  }
+    setTodo((todo) => ({
+      ...todo,
+      text: event.target.value,
+      id: shortid.generate(),
+      isCompleted: false,
+    }));
+  };
+  console.log(todo);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo.text.trim().length > 0) {
+      setList((list) => [...list, todo]);
+    }
+  };
+
   console.log(list);
 
   return (
-    <div className = "app">
-     <header id ="header">
-       TO-DO LIST 
-     </header>
-     <form onSubmit={handleSubmit}>
-       <input placeholder="Enter Here"
-              onChange = {handleChange}
-       ></input>
-       <button onClick={handleSubmit}  >+</button>
-     </form>
+    <div className="app">
+      <header id="header">TO-DO LIST</header>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Enter Here"
+          onChange={handleChange}
+          value={todo.text}
+        ></input>
+        <button onClick={handleSubmit}>+</button>
+      </form>
     </div>
   );
 }
